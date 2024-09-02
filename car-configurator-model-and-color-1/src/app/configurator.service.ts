eef8f8c9f106de0da9e84de48330d9c049390120
haze272 <studiorain27@gmail.com>
2024-09-02 17:21:31 +0300

    feat(challenge): 9_1 done/complete

--- car-configurator-model-and-color-1/src/app/configurator.service.ts
+++ car-configurator-model-and-color-1/src/app/configurator.service.ts
@@ -1,7 +1,7 @@
-import {inject, Injectable, signal, Signal} from '@angular/core';
+import {computed, inject, Injectable, signal, Signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {CarModel} from './models.type';
+import {CarModel, Color, SelectedConfig} from './models.type';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,18 @@ export class ConfiguratorService {
   );
 
   readonly currentCar = signal<CarModel | undefined>(undefined);
+  readonly currentColor = signal<Color | undefined>(undefined);
 
+  readonly selectableColors = computed(() => this.currentCar()?.colors);
+
+  selectModel(code: CarModel["code"]) {
+    const model = this.allModels().find(model => model.code === code);
+    this.currentCar.set(model);
+    this.currentColor.set(model?.colors[0]);
+  }
+
+  selectColor(code: Color["code"]) {
+    const color = this.selectableColors()?.find(color => color.code === code);
+    this.currentColor.set(color);
+  }
 }
--- car-configurator-model-and-color-1/src/app/step1/step1.component.ts
+++ car-configurator-model-and-color-1/src/app/step1/step1.component.ts
@@ -1,15 +1,20 @@
-import {Component, inject} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {Component, computed, inject, model} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ConfiguratorService} from "../configurator.service";
+import {CarModel, Color} from "../models.type";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-step1',
   standalone: true,
-    imports: [
-        ReactiveFormsModule
-    ],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    JsonPipe
+  ],
   templateUrl: './step1.component.html',
   styleUrl: './step1.component.scss'
 })
 export class Step1Component {
-
+  configuratorService = inject(ConfiguratorService);
 }
