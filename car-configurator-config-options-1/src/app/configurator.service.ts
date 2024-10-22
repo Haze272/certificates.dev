44c40ccb42aee3800cf4293c16b6f896efedce58
haze272 <studiorain27@gmail.com>
2024-10-22 14:03:57 +0300

    feat(challenges): add 9.3

--- car-configurator-config-options-1/src/app/configurator.service.ts
+++ car-configurator-config-options-1/src/app/configurator.service.ts
@@ -1,57 +1,64 @@
-import {computed, inject, Injectable, signal, Signal} from '@angular/core';
+import {computed, effect, inject, Injectable, signal, Signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {CarModel, CarOptions, Color, Config} from './models.type';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ConfiguratorService {
 
-  private http = inject(HttpClient);
-  readonly allModels: Signal<CarModel[]> = toSignal(
-    this.http.get<CarModel[]>("models"), {initialValue: []}
-  );
+    private http = inject(HttpClient);
+    readonly allModels: Signal<CarModel[]> = toSignal(
+        this.http.get<CarModel[]>("models"), {initialValue: []}
+    );
 
-  readonly selectableColors = computed(() => this.currentCar()?.colors);
+    readonly selectableColors = computed(() => this.currentCar()?.colors);
 
-  readonly currentColor = signal<Color | undefined>(undefined);
-  readonly currentCar = signal<CarModel | undefined>(undefined);
+    readonly currentColor = signal<Color | undefined>(undefined);
+    readonly currentCar = signal<CarModel | undefined>(undefined);
+    readonly currentImage = computed(
+        () => {
+            const car = this.currentCar();
+            const color = this.currentColor();
+            if (car && color)
+                return `https://interstate21.com/tesla-app/images/${car.code}/${color.code}.jpg`
+            else return null;
+        }
+    );
 
-  readonly currentImage = computed(
-    () => {
-      const car = this.currentCar();
-      const color = this.currentColor();
-      if (car && color)
-        return `https://interstate21.com/tesla-app/images/${car.code}/${color.code}.jpg`
-      else return null;
+    readonly step2Ready = computed(() => (this.currentCar() != undefined) && (this.currentColor() != undefined));
+
+    readonly currentCarOptions: Signal<CarOptions | null> = signal(null);
+
+    readonly currentCarConfig = signal<Config | undefined>(undefined);
+
+    constructor() {
+      effect(() => {
+        if (this.currentCar()?.code) {
+          this.http.get<CarOptions>(`options/${this.currentCar()?.code}`)
+              .subscribe(options => this.currentCarOptions.set())
+        }
+      });
     }
-  );
 
-  readonly currentCarOptions: Signal<CarOptions | undefined> = computed(() => {
-    return toSignal(
-        this.http.get<CarOptions>(`options/${this.currentCar()?.code}`),
-        {initialValue: undefined}
-    )();
-  });
+    selectModel(code: CarModel["code"]) {
+        const model = this.allModels().find(model => model.code === code);
+        this.currentCar.set(model);
+        this.currentColor.set(model?.colors[0]);
+    }
 
-  readonly currentCarConfig = signal<Config | undefined>(undefined);
+    selectColor(code: Color["code"]) {
+        const color = this.selectableColors()?.find(color => color.code === code);
+        this.currentColor.set(color);
+    }
 
-  selectModel(code: CarModel["code"]) {
-    const model = this.allModels().find(model => model.code === code);
-    this.currentCar.set(model);
-    this.currentColor.set(model?.colors[0]);
-  }
-
-  selectColor(code: Color["code"]) {
-    const color = this.selectableColors()?.find(color => color.code === code);
-    this.currentColor.set(color);
-  }
-
-  selectCarOption(carConfigId: Config['id']) {
-    const carConfig = this.currentCarOptions()?.configs.find(carConfig => {
-      return carConfig.id === carConfigId;
-    });
-    this.currentCarConfig.set(carConfig);
-  }
+    selectCarOption(carConfigId: Config['id']) {
+        console.log('selectCarOption')
+        const carConfig = this.currentCarOptions()?.configs.find(carConfig => {
+            return carConfig.id === carConfigId;
+        });
+        console.log('carConfig:', carConfig)
+        this.currentCarConfig.set(carConfig);
+    }
 }
